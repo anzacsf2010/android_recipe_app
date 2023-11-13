7ecde08fc496d56b02b2f42c94f20973a6198037
Andy St Fort <andy.st.fort@niceincontact.com>
2023-11-12 21:06:43 -0400

    Update - Bug fixes  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app/src/main/java/com/training/myrecipeapp/CategoryDetailScreen.kt

--- app/src/main/java/com/training/myrecipeapp/CategoryDetailScreen.kt
+++ app/src/main/java/com/training/myrecipeapp/CategoryDetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -28,7 +29,7 @@ fun CategoryDetailScreen(category : Category) {
             painter = rememberAsyncImagePainter(model = category.strCategoryThumb),
             contentDescription = null,
             modifier = Modifier
-                .fillMaxSize()
+                .wrapContentSize()
                 .aspectRatio(1f)
         )
         Text(
